0df7b82f23e7e9404b36b76f8381d2cd8798a7d3
Fani-19 <estefbd@hotmail.com>
2020-07-29 18:09:34 +0200

    add juego Adivina numero

---
+++ src/com/company/Aplicacion.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Aplicacion {
+
+    public static void main(String[] args) {
+
+    JuegoAdivinaNumero juego=new JuegoAdivinaNumero(5,6);
+    juego.juega();
+
+    }
+}
+
---
+++ src/com/company/JuegoAdivinaNumero.java
@@ -0,0 +1,43 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class JuegoAdivinaNumero extends Juego {
+    private int numAdivinar;
+
+    public JuegoAdivinaNumero(int vidas, int num) {
+        super(vidas);
+        this.numAdivinar = num;
+    }
+
+    @Override
+    public void juega() {
+        this.reiniciaPartida();
+        int num;
+        Scanner leer = new Scanner(System.in);
+        boolean seguir = true;
+        do {
+            System.out.println("Adivina el número entre 0 y 10");
+            num = leer.nextInt();
+            leer.nextLine();
+            if (numAdivinar == num) {
+                System.out.println("Acertaste!!");
+                actualizaRecord();
+                seguir = false;
+            } else {
+                if (quitaVida()) {
+                if (num > numAdivinar) {
+                    System.out.println("El número es menor, vuelve a intentarlo!!");
+                } else {
+
+                    System.out.println("El números es mayor, vuelve a intentarlo!!");
+                }
+                }else{
+                    seguir = false;
+                }
+            }
+        } while (seguir);
+
+    }
+
+}
