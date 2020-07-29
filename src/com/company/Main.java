585495a9fd3ef1a5ee3938b92ecc4332d3b3e933
Fani-19 <estefbd@hotmail.com>
2020-07-29 17:27:24 +0200

    Comienzo proyecto y ejercios practicas 2 completada

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,16 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Juego juego = new Juego(5);
+        Juego juego1 = new Juego(5);
+        juego.quitaVida();
+        juego.MuestraVidasRestantes();
+        juego.reiniciaPartida();
+        juego.MuestraVidasRestantes();
+        juego.actualizaRecord();
+        juego1.actualizaRecord();
+
     }
 }
+
